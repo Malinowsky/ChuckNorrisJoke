792b0bfaf9dd6feb7859579deb8640b856dca249
Malinowsky <arturrmalinowski@gmail.com>
2022-01-13 18:21:42 +0100

    ArtMal

---
+++ src/test/java/artmal/springframework/ChuckNorrisJoke/ChuckNorrisJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package artmal.springframework.ChuckNorrisJoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
